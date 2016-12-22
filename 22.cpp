a63a0ca2597a11c576334cf46191efa408fe06b8
ciec20151104710 <2471388619@qq.com>
2016-12-22 20:20:14 +0800

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  链表2-
+//
+//  Created by s20151104710 on 16/10/17.
+//  Copyright © 2016年 s20151104710. All rights reserved.
+//
+
+#include <iostream>
+#include<stdio.h>
+#define N 5
+struct Node
+{
+    char name[40];
+    int age;
+    struct Node *next;
+};
+int main()
+{
+    struct Node *head,*p,*q;
+    int i;
+    head=new Node;
+    head->next=0;
+    q=head;
+    scanf("%s",head->name);
+    scanf("%d",&head->age);
+    for(i=0;i<N;i++)
+    {
+        p=new Node;
+        scanf("%s",p->name);
+        scanf("%d",&p->age);
+        p->next=0;
+        q->next=p;
+        q=p;
+    }
+    p=head;
+    while(p)
+    {
+        printf("%s   %d  \n",p->name,p->age);
+        p=p->next;
+    }
+    return 0;
+}
